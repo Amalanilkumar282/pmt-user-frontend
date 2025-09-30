a9b3dd037ef24901fab2debf015d26cf986596e7
Amalanilkumar282 <amalanilkumar282@gmail.com>
2025-09-30 23:39:14 +0530

    backlog-page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BacklogPage } from './backlog/backlog-page/backlog-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/backlog', pathMatch: 'full' },
+  { path: 'backlog', component: BacklogPage }
+];
