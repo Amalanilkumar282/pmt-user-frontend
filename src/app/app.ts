7d6c2b825f68c118e595d66d0598d993476fefe1
NadimNaisam <nadimvc@gmail.com>
2025-10-01 13:22:48 +0530

    created modal for issues

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateIssue } from './modal/create-issue/create-issue';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateIssue],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
