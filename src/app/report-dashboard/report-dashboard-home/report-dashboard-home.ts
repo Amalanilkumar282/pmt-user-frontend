c6f015d978eb5143973438e8084350dd8726bbcb
Kavya17012003 <lbt21cs056@gmail.com>
2025-10-01 12:06:20 +0530

    added report dashboard screen

--- src/app/report-dashboard/report-dashboard-home/report-dashboard-home.ts
+++ src/app/report-dashboard/report-dashboard-home/report-dashboard-home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Navbar } from '../../shared/navbar/navbar';
+import { Sidebar } from '../../shared/sidebar/sidebar';
+import { SidebarStateService } from '../../shared/services/sidebar-state.service';
 
 @Component({
   selector: 'app-report-dashboard-home',
-  imports: [],
+  standalone: true,
+  imports: [Navbar,Sidebar],
+  providers: [SidebarStateService], 
   templateUrl: './report-dashboard-home.html',
   styleUrl: './report-dashboard-home.css'
 })
 export class ReportDashboardHome {
 
+  private sidebarStateService = inject(SidebarStateService);
+  isSidebarCollapsed = this.sidebarStateService.isCollapsed;
+
+  onToggleSidebar(): void {
+    this.sidebarStateService.toggleCollapse();
+  }
 }
