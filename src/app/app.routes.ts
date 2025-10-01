b06d2ffae65b83e0535eea58bb228f36c54a5dea
Sharath Shony <sharath.shony@experionglobal.local>
2025-10-01 17:00:19 +0530

    Added timeline component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BacklogPage } from './backlog/backlog-page/backlog-page';
 import { SummaryPage } from './summary/summary-page/summary-page';
 import { ReportDashboardHome } from './report-dashboard/report-dashboard-home/report-dashboard-home';
+import { TimelineComponent } from './timeline/timeline-component/timeline-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/summary', pathMatch: 'full' },
   { path: 'summary', component: SummaryPage },
   { path: 'backlog', component: BacklogPage },
-  {path:'report-dashboard',component:ReportDashboardHome}
+  {path:'report-dashboard',component:ReportDashboardHome},
+  {path:'timeline',component:TimelineComponent}
 ];
