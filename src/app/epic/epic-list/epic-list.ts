1eed1bb8a62a50899bee35a3dab90e2d4cbe89e2
Amalanilkumar282 <amalanilkumar282@gmail.com>
2025-10-06 12:37:25 +0530

    created epic list and epic detailed view components

---
+++ src/app/epic/epic-list/epic-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epic-list',
+  imports: [],
+  templateUrl: './epic-list.html',
+  styleUrl: './epic-list.css'
+})
+export class EpicList {
+
+}
