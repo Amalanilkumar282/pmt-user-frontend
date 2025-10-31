d096ea98686d3b0adc4ecd0128b294d3a825af46
NadimNaisam <nadimvc@gmail.com>
2025-10-31 11:31:18 +0530

    Integrated APIs for create label, edit label, and get labels

---
+++ src/app/shared/services/label.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Label {
+  id: number;
+  name: string;
+  colour: string;
+}
+
+export interface LabelResponse {
+  status: number;
+  data: Label[];
+  message: string;
+}
+
+export interface CreateLabelRequest {
+  name: string;
+  colour: string;
+}
+
+export interface UpdateLabelRequest {
+  id: number;
+  name: string;
+  colour: string;
+}
+
+export interface CreateLabelResponse {
+  status: number;
+  data: Label;
+  message: string;
+}
+
+export interface UpdateLabelResponse {
+  status: number;
+  data: Label;
+  message: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class LabelService {
+  private baseUrl = '/api/Label';
+
+  constructor(private http: HttpClient) {}
+
+  getAllLabels(): Observable<LabelResponse> {
+    const token = sessionStorage.getItem('accessToken') || '';
+    const headers = { 'Authorization': `Bearer ${token}` };
+    return this.http.get<LabelResponse>(this.baseUrl, { headers });
+  }
+
+  createLabel(request: CreateLabelRequest): Observable<CreateLabelResponse> {
+    const token = sessionStorage.getItem('accessToken') || '';
+    const headers = { 'Authorization': `Bearer ${token}` };
+    return this.http.post<CreateLabelResponse>(this.baseUrl, request, { headers });
+  }
+
+  updateLabel(request: UpdateLabelRequest): Observable<UpdateLabelResponse> {
+    const token = sessionStorage.getItem('accessToken') || '';
+    const headers = { 'Authorization': `Bearer ${token}` };
+    return this.http.put<UpdateLabelResponse>(this.baseUrl, request, { headers });
+  }
+}
